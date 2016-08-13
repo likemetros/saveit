e08dbf8aae3e6c4027186ab7a559f3f85525f7fe
likemetros <likemetros@gmail.com>
2016-08-13 22:14:36 +0200

    v0.2

---
+++ register.php
@@ -0,0 +1,14 @@
+
+<html>
+	<head>
+		<title>SaveIt</title>
+	</head>
+	<body>
+		<form action="reg.php" method="post">
+			<p>Username:<input type="text" name="username"/></p>
+			<p>Password:<input type="text" name="password"/></p>
+			<p><input type="submit" name="check" value="Check"/></p>
+			<p><input type="submit" name="reg" value="Register"/></p>
+		</form>
+	</body>
+</html>
