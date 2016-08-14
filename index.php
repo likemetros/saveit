c737637bb9304b95c9215a5d88b68b7aca413da2
likemetros <likemetros@gmail.com>
2016-08-14 09:34:36 +0200

    Text saving and text preview done.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,15 +10,53 @@ session_start();
 	<body>
 <?php
 	if (isset($_SESSION["username"])){
+		$name=$_POST["username"];
+		$name=$_SESSION["username"];
 ?>
+<table width="100%">
+	<tr>
+		<td width="40%">
 		<form method="post">
 			<input type="hidden" name="logoutbag" value="1"/>
 			<p align="right" valign="top"><input type="submit" name="logout" value="Log out"/></p>
 		</form>
+		<!--WRITING-->
+		<form method="post">
+			<p align="left" valign="top"><textarea name="text" rows="15" cols="80"></textarea></p>
+			<p ><input type="submit" name="write" value="Submit text"/></p>
+		</form>
+		<form method="post">
+			<input type="hidden" name="refresh" value="1"/>
+			<p ><input type="submit" name="rfr" value="Refresh"/></p>
+		</form>
+		</td>
+		<td width="60%">
+			<p align="left" valign="top"><h2>Preview</h2></p>
+			<?php
+				$read=file("accs/".$name.".txt");
+				$count=count($read);
+				$i=1;
+				foreach($read as $line) {
+					echo $line;
+					if ($i<$count){
+						echo"\r\n";
+					}
+					$i++;
+				}
+			?>
+		</td>
+	</tr>
+</table>
 <?php
 		if ($_POST["logoutbag"]=="1"){
 			session_destroy();
 		}
+		if ($_POST["rfr"]=="1"){
+			header("Refresh:0");
+		}
+		$wf=fopen("accs/".$name.".txt","a");
+		fwrite($wf,"\r\n".$_POST["text"]);
+		fclose($wf);
 	} else {
 ?>
 		<div>
